5f280a6834fe925fbc65c32210109eae80d0e2a0
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-24 08:54:19 +0200

    Initial Commit

---
+++ GuessTheFlagApp/GuessTheFlagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAppApp.swift
+//  GuessTheFlagApp
+//
+//  Created by Buhle Radzilani on 2024/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
